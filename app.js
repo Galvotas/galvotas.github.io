fc26308fa2e99df34c6502e56cb5a303ae4b02f4
antanas andrulis <a.andrulis@yahoo.com>
2021-05-02 16:58:27 +0300

    added one more project, removed unncecessary skills

--- app.js
+++ app.js
@@ -1,228 +1,234 @@
+const skillCards = document.querySelector(".skill-cards");
+const cardsContainer = document.querySelector(".cards");
+const date = document.querySelector("#date");
+const headerButtons = document.querySelector(".header-btns");
+const heroDot = document.querySelector(".dot");
 
-const skillCards = document.querySelector('.skill-cards');
-const cardsContainer = document.querySelector('.cards');
-const date = document.querySelector('#date')
-const headerButtons = document.querySelector('.header-btns')
-const heroDot = document.querySelector('.dot')
-
-
-
-date.innerHTML = new Date().getFullYear()
+date.innerHTML = new Date().getFullYear();
 
 let textToDisplay;
-let cards = []
+let cards = [];
 
 const projectData = [
-    {
-        id: "0",
-        used: "Reviewing Landlords",
-        github: '',
-        challenges: "Reviewing landlords from Lithuania made possible and easy."
-    },
-    {
-        id: "1",
-        used: "Barbershop For Men",
-        github: 'https://github.com/Galvotas/barbershop.git',
-        challenges: "A modern barbershop for modern people. Client can see barbers and their work, and so make first impression.",
-    },
-    
-    {
-        id: "3",
-        used: "Simple Recipes",
-        github: 'https://github.com/Galvotas/Recipes-React.git',
-        challenges: "Simple recipes website made with React."
-    },
-    {
-        id: "4",
-        used: "For Tour Lovers",
-        github: 'https://github.com/Galvotas/peskom.git',
-        challenges: 'Static website made for tour enthusiast. Designed by Mališauskas studio.'
-        }
-    ]
+  {
+    id: "0",
+    used: "Reviewing Landlords",
+    github: "",
+    challenges: "Reviewing landlords from Lithuania made possible and easy.",
+  },
+  {
+    id: "1",
+    used: "Barbershop For Men",
+    github: "https://github.com/Galvotas/barbershop.git",
+    challenges:
+      "A modern barbershop for modern people. Client can see barbers and their work, and so make first impression.",
+  },
+
+  {
+    id: "3",
+    used: "Simple Recipes",
+    github: "https://github.com/Galvotas/Recipes-React.git",
+    challenges: "Simple recipes website made with React.",
+  },
+  {
+    id: "4",
+    used: "For Tour Lovers",
+    github: "https://github.com/Galvotas/peskom.git",
+    challenges:
+      "Static website made for tour enthusiast. Designed by Mališauskas studio.",
+  },
+  {
+    id: "5",
+    used: "C19 & it's vaccination tracking",
+    github: "https://github.com/Galvotas/Kilo-PR.git",
+    challenges:
+      "COVID19 pandemic and it's vaccination tracking with interactive map and charts.",
+  },
+];
 const skillsData = [
-    {
-skillName: "HTML",
-iconUrl: "icons/html-5.svg"
-    },
-    {
-        skillName: "CSS",
-        iconUrl: "icons/css-3.svg"
-    },
-    {
-        skillName: "JAVASCRIPT",
-        iconUrl: "icons/js.svg"
-    },
-    {
-        skillName: "REACT",
-        iconUrl: "icons/react.svg"
-    },
-    {
-        skillName: "REACT HOOKS",
-        iconUrl: "icons/hooks.svg"
-    },
-    {
-        skillName: "FORMIK",
-        iconUrl: ""
-    },
-    {
-        skillName: "YUP",
-        iconUrl: ""
-    },
-    {
-        skillName: "STYLED",
-        iconUrl: "icons/styled_components.svg"
-    },
-    {
-        skillName: "SASS",
-        iconUrl: "icons/sass.svg"
-    },
-    {
-        skillName: "MATERIAL-UI",
-        iconUrl: "icons/letter-m.svg"
-    },
-    {
-        skillName: "MATERIALIZE CSS",
-        iconUrl: ""
-    },
-    {
-        skillName: "BOOTSTRAP",
-        iconUrl: "icons/bootstrap.svg"   
-    },
-    {
-        skillName: "REDUX",
-        iconUrl: "icons/redux.svg"
-    },
-    {
-        skillName: "GIT",
-        iconUrl: "icons/git.svg"
-    },
-    {
-        skillName: "STRAPI",
-        iconUrl: ""
-    },
-    {
-        skillName: "RESPONSIVE DESIGN",
-        iconUrl: "icons/responsive-design-symbol.svg"
-    },
-    
-]
+  {
+    skillName: "HTML",
+    iconUrl: "icons/html-5.svg",
+  },
+  {
+    skillName: "CSS",
+    iconUrl: "icons/css-3.svg",
+  },
+  {
+    skillName: "SASS",
+    iconUrl: "icons/sass.svg",
+  },
+  {
+    skillName: "JAVASCRIPT",
+    iconUrl: "icons/js.svg",
+  },
+  {
+    skillName: "TYPESCRIPT",
+    iconUrl: "",
+  },
+  {
+    skillName: "REACT",
+    iconUrl: "icons/react.svg",
+  },
+  {
+    skillName: "REDUX",
+    iconUrl: "icons/redux.svg",
+  },
+  {
+    skillName: "FORMIK",
+    iconUrl: "",
+  },
+  {
+    skillName: "YUP",
+    iconUrl: "",
+  },
+  {
+    skillName: "STYLED-COMPONENTS",
+    iconUrl: "icons/styled_components.svg",
+  },
+  {
+    skillName: "MATERIAL-UI",
+    iconUrl: "icons/letter-m.svg",
+  },
+  {
+    skillName: "MATERIALIZE CSS",
+    iconUrl: "",
+  },
+  {
+    skillName: "BOOTSTRAP",
+    iconUrl: "icons/bootstrap.svg",
+  },
+  {
+    skillName: "FIREBASE",
+    iconUrl: "",
+  },
+  {
+    skillName: "STRAPI",
+    iconUrl: "",
+  },
+];
 
 function check(element, classname, link) {
-    if(element.target.classList.contains(`${classname}`))  {
-        window.open(`${link}`, '_blank');
-    }
+  if (element.target.classList.contains(`${classname}`)) {
+    window.open(`${link}`, "_blank");
+  }
 }
 
 function check(element, classname, link) {
-    if(element.target.classList.contains(`${classname}`))  {
-        window.open(`${link}`, '_blank');
-    }
+  if (element.target.classList.contains(`${classname}`)) {
+    window.open(`${link}`, "_blank");
+  }
 }
 
 const displaySkills = () => {
-    skillsData.forEach(skill => {
-        const {skillName, iconUrl} = skill
-        const card = document.createElement('div')
-        card.classList.add('skill-card')
-        const skillIconContainer = document.createElement('div')
-        skillIconContainer.classList.add('skill-icon')
-const skillIcon = document.createElement('img')
-skillIcon.src = `${iconUrl}`
-const skillType = document.createElement('h3')
-skillType.classList.add('skill-type')
-skillCards.appendChild(card)
-card.appendChild(skillIconContainer)
-skillIconContainer.appendChild(skillIcon)
-card.appendChild(skillType)
-skillType.innerHTML = `${skillName}`
-    })
-}
+  skillsData.forEach((skill) => {
+    const { skillName, iconUrl } = skill;
+    const card = document.createElement("div");
+    card.classList.add("skill-card");
+    const skillIconContainer = document.createElement("div");
+    skillIconContainer.classList.add("skill-icon");
+    const skillIcon = document.createElement("img");
+    skillIcon.src = `${iconUrl}`;
+    const skillType = document.createElement("h3");
+    skillType.classList.add("skill-type");
+    skillCards.appendChild(card);
+    card.appendChild(skillIconContainer);
+    skillIconContainer.appendChild(skillIcon);
+    card.appendChild(skillType);
+    skillType.innerHTML = `${skillName}`;
+  });
+};
 
 const createProjectCard = (githubLink, index) => {
-    const card = document.createElement('div')
-    card.className = `card card${index}`
-    card.setAttribute('data-key', index)
-    let c = `
+  const card = document.createElement("div");
+  card.className = `card card${index}`;
+  card.setAttribute("data-key", index);
+  let c = `
     <div class="project-description"></div>
-    ${githubLink && `
+    ${
+      githubLink &&
+      `
     <div class="github-link">
       <a
         href="${githubLink}"
         target="_blank"><i class="fab fa-github"></i></a>
     </div>
-`}
-    `
-    card.innerHTML = c
-    cardsContainer.appendChild(card)
-    cards.push(card)
-}
+`
+    }
+    `;
+  card.innerHTML = c;
+  cardsContainer.appendChild(card);
+  cards.push(card);
+};
 
 const createCards = () => {
-    projectData.map((project, index) => {
-        const {github} = project
-        return createProjectCard(github, index)
-    })
-}
+  projectData.map((project, index) => {
+    const { github } = project;
+    return createProjectCard(github, index);
+  });
+};
 
 const cardEnter = (cards) => {
-cards.forEach(card => card.addEventListener('mouseenter', (e) => {
-    const descriptionElement = e.target.children[0]
-    let cardNumber = e.target.dataset.key
-    descriptionElement.style.width = "80%"
-    descriptionElement.style.visibility = "visible"
-    descriptionElement.innerHTML = ""
-setTimeout(() => {
-descriptionElement.innerHTML = `
+  cards.forEach((card) =>
+    card.addEventListener("mouseenter", (e) => {
+      const descriptionElement = e.target.children[0];
+      let cardNumber = e.target.dataset.key;
+      descriptionElement.style.width = "80%";
+      descriptionElement.style.visibility = "visible";
+      descriptionElement.innerHTML = "";
+      setTimeout(() => {
+        descriptionElement.innerHTML = `
 <h3 class="description-heading">${projectData[cardNumber].used}</h3>
 <p class="description-text">${projectData[cardNumber].challenges}</p>
-`
-}, 350)
-}))
-}
+`;
+      }, 350);
+    })
+  );
+};
 
 const cardLeave = (cards) => {
-    cards.forEach(card => card.addEventListener('mouseleave', (e) => {
-        const descriptionElement = e.target.children[0]
-        descriptionElement.style.width = "0"
-        descriptionElement.innerHTML = ""
-        descriptionElement.style.visibility = 'hidden'
-    }))
-}
+  cards.forEach((card) =>
+    card.addEventListener("mouseleave", (e) => {
+      const descriptionElement = e.target.children[0];
+      descriptionElement.style.width = "0";
+      descriptionElement.innerHTML = "";
+      descriptionElement.style.visibility = "hidden";
+    })
+  );
+};
 
 const doThings = async () => {
-    await createCards()
-    cardEnter(cards)
-    cardLeave(cards)
-}
+  await createCards();
+  cardEnter(cards);
+  cardLeave(cards);
+};
 
+window.addEventListener("DOMContentLoaded", () => {
+  displaySkills();
+  doThings();
+});
 
+window.addEventListener("click", (e) => {
+  check(e, `card0`, `https://tanuoma.lt/`);
+  check(e, `card3`, `https://peskom.lt/`);
+  check(e, `card1`, `https://galvotas.github.io/barbershop/`);
+  check(e, `card2`, `https://galvotas.github.io/Restaurant/`);
+  check(e, `card4`, `https://covidinho-892bd.web.app/`);
+});
 
-window.addEventListener('DOMContentLoaded', () => {
-    displaySkills()
-    doThings()
-})
-
-window.addEventListener('click', (e) => {
-    check(e, `card0`, `https://tanuoma.lt/`)
-check(e, `card3`, `https://peskom.lt/` )
-check(e, `card1`, `https://galvotas.github.io/barbershop/`)
-check(e, `card2`, `https://galvotas.github.io/Restaurant/`)
-})
-
-headerButtons.addEventListener('mouseover', e => {
-    if(e.target.classList.contains('portfolio-btn') || e.target.classList.contains('contact-btn')) {
-        heroDot.classList.toggle('changeDotColor')
-    }
-})
-headerButtons.addEventListener('mouseout', e => {
-    if(e.target.classList.contains('portfolio-btn') || e.target.classList.contains('contact-btn')) {
-        heroDot.classList.remove('changeDotColor')
-
-    }
-})
-
-
-
-
-
-
+headerButtons.addEventListener("mouseover", (e) => {
+  if (
+    e.target.classList.contains("portfolio-btn") ||
+    e.target.classList.contains("contact-btn")
+  ) {
+    heroDot.classList.toggle("changeDotColor");
+  }
+});
+headerButtons.addEventListener("mouseout", (e) => {
+  if (
+    e.target.classList.contains("portfolio-btn") ||
+    e.target.classList.contains("contact-btn")
+  ) {
+    heroDot.classList.remove("changeDotColor");
+  }
+});
